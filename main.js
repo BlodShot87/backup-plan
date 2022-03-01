df1929fd7ad07e5aff7201601f4447e7e25b0947
BlodShot87 <92914340+BlodShot87@users.noreply.github.com>
2022-03-01 19:24:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var apple_app= "My first console project"
+console.log(apple_app)
+var first_name= "Ranbir";
+var last_name= "Kapoor";
+var full_name=first_name.concat(last_name);
+console.log(full_name)
+ var a = 10;
+ var b = 5;
+ var add = a + b ;
+ console.log(add)
+ console.log(b)
+ var c = 25;
+ var d = 25;
+ var added = c + d ;
+ console.log(added)
+ var e = 2;
+ console.log(e)
